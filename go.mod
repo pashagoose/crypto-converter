a36bce326d76cfbe0c376d92cd9634ac06c206a1
guskov.pavel3 <guskov.pavel3@wb.ru>
2025-08-31 16:59:26 +0300

    Revert "finish crypto converter CLI"
    This reverts commit ed13d523e470986e5aff68e27e58c4e0d6cd0048.

--- go.mod
+++
@@ -1,5 +0,0 @@
-module crypto-converter
-
-go 1.21
-
-require github.com/joho/godotenv v1.5.1
--- internal/domain/repositories/currency_repository.go
+++
@@ -1,9 +0,0 @@
-package repositories
-
-import (
-	"context"
-)
-
-type CurrencyRepository interface {
-	GetExchangeRate(ctx context.Context, from, to string) (float64, error)
-}
